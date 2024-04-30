425df48bd0bb6349a89b21161af44d43c20e12db
JavitoRoGo <130165792+JavitoRoGo@users.noreply.github.com>
2024-04-30 12:56:37 +0100

    Initial Commit

---
+++ AllAboutYourSeries/AllAboutYourSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutYourSeriesApp.swift
+//  AllAboutYourSeries
+//
+//  Created by Javier Rodríguez Gómez on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutYourSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
